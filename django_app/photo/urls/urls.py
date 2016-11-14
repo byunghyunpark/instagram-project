63554eab12f63815acb6e4d0df8cc12e862826dc
byunghyunpark <openmind8735@gmail.com>
2016-11-14 22:58:58 +0900

    photo detail 페이지 추가 및 댓글 기능 추가

--- django_app/photo/urls/urls.py
+++ django_app/photo/urls/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
+from photo.views import PhotoDisplayView
 from .. import views
 
 urlpatterns = [
-    url(r'^list/$', views.PhotoList.as_view(), name='photo_list'),
-    url(r'^add/$', views.PhotoAdd.as_view(), name='photo_add'),
+    url(r'^photo/$', views.PhotoList.as_view(), name='photo_list'),
+    url(r'^photo/add/$', views.PhotoAdd.as_view(), name='photo_add'),
+    url(r'^photo/(?P<pk>[0-9]+)/$', views.PhotoDetail.as_view(), name='photo_detail'),
 ]
--- django_app/photo/views.py
+++ django_app/photo/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic import DetailView
+from django.views.generic import FormView
 from django.views.generic import ListView, CreateView
+from django.views.generic.detail import SingleObjectMixin
 
-from photo.models import Photo
+from django import forms
+from photo.models import Photo, PhotoComment
 
 
 class PhotoList(ListView):
@@ -22,3 +30,49 @@ class PhotoAdd(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(PhotoAdd, self).form_valid(form)
+
+
+class PhotoDisplayView(DetailView):
+    """
+    PhotoDetail에서 get요청이 온 경우
+    """
+    model = Photo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PhotoCommentForm()
+        return context
+
+
+class PhotoCommentForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea)
+
+
+class PhotoCommentFormView(SingleObjectMixin, FormView):
+    template_name = 'photo/photo_detail.html'
+    form_class = PhotoCommentForm
+    model = Photo
+
+    def form_valid(self, form):
+        self.object = self.get_object()
+        content = form.cleaned_data['content']
+        PhotoComment.objects.create(
+            photo=self.object,
+            author=self.request.user,
+            content=content,
+        )
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('photo:photo_detail', kwargs={'pk': self.object.pk})
+
+
+class PhotoDetail(View):
+
+    def get(self, request, *args, **kwargs):
+        view = PhotoDisplayView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = PhotoCommentFormView.as_view()
+        return view(request, *args, **kwargs)
