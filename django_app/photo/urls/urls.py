86ff90673125a178883e70d3f4ccf5ac4d8ab7eb
byunghyunpark <openmind8735@gmail.com>
2016-11-14 15:33:19 +0900

    class view로 photo list 리팩토링

--- django_app/photo/urls/urls.py
+++ django_app/photo/urls/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .. import views
 
 urlpatterns = [
-    url(r'^list/$', views.photo_list, name='photo_list'),
+    # url(r'^list/$', views.photo_list, name='photo_list'),
+    url(r'^photo/$', views.PhotoList.as_view(), name='photo_list')
 ]
--- django_app/photo/views.py
+++ django_app/photo/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from photo.models import Photo
 
 
-def photo_list(request):
-    photos = Photo.objects.all()
-    context = {
-        'photos': photos,
-    }
-    return render(request, 'photo_list.html', context)
+# def photo_list(request):
+#     photos = Photo.objects.all()
+#     context = {
+#         'photos': photos,
+#     }
+#     return render(request, 'photo_list.html', context)
+
+
+class PhotoList(ListView):
+    model = Photo
+    context_object_name = 'photos'
+    paginate_by = 3
+
